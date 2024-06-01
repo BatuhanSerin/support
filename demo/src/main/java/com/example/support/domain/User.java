63b58d742b576ae0794780096cfa808f428cc618
batuhanserin <batuhanserin97@gmail.com>
2024-06-01 14:23:57 +0300

    set annotations for jpa datatable for User

--- demo/src/main/java/com/example/support/domain/User.java
+++ demo/src/main/java/com/example/support/domain/User.java
@@ -3,7 +3,24 @@ package com.example.support.domain;
 import java.io.Serializable;
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String userId;
     private String firstName;
